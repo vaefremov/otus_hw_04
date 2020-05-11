db290291744fff3e73cb7bc981e675c2b421c37f
Vladimir Efremov <vefremov@gmail.com>
2020-05-11 20:11:35 +0200

    Added tests for iterable containers

--- include/output_ip.h
+++ include/output_ip.h
@@ -6,11 +6,33 @@
 namespace OTUS
 {
 
+
+namespace detail
+{
+    using std::begin;
+    using std::end;
+ 
+    template <typename T>
+    auto is_iterable_impl(int)
+    -> decltype (
+        begin(std::declval<T&>()) != end(std::declval<T&>()), // begin/end and operator !=
+        ++std::declval<decltype(begin(std::declval<T&>()))&>(), // operator ++
+        *begin(std::declval<T&>()), // operator*
+        std::true_type{});
+ 
+    template <typename T>
+    std::false_type is_iterable_impl(...);
+ 
+}
+ 
+template <typename T>
+using is_iterable = decltype(detail::is_iterable_impl<T>(0));
+
+
 template<typename T>
 std::enable_if_t<std::is_integral_v<T>, void>
 print_ip(T ip, std::ostream& out, bool add_endline = true)
 {
-    // out << __PRETTY_FUNCTION__ << " " << sizeof(ip) << " " << ip;
     char* ip_bytes = reinterpret_cast<char*>(&ip);
     for (size_t i = 0; i < sizeof(ip); i++)
     {
@@ -23,12 +45,36 @@ print_ip(T ip, std::ostream& out, bool add_endline = true)
 }
 
 template<typename T>
-std::enable_if_t<std::is_same_v<T, std::string>, void>
-print_ip(T ip, std::ostream& out, bool add_endline = true)
+std::enable_if_t<is_iterable<T>::value, void>
+print_ip(T const& ip, std::ostream& out, bool add_endline = true)
+{
+    bool starting{true};
+    for(auto const & v: ip)
+    {
+        if(!starting)
+            out << ".";
+        else
+            starting = false;        
+        out << v;
+    }
+    if(add_endline)
+        out << std::endl;
+}
+
+template<>
+void print_ip<std::string>(std::string const& ip, std::ostream& out, bool add_endline)
 {
     out <<  ip;
     if(add_endline)
         out << std::endl;
 }
+
+inline void print_ip(std::tuple<int, int, int, int> const& ip, std::ostream& out, bool add_endline = true)
+{
+    out << std::get<0>(ip) << "." << std::get<1>(ip) << "." << std::get<2>(ip) << "." << std::get<3>(ip);
+    if(add_endline)
+        out << std::endl;
+}
+
 
 }
---
+++ tests/test_iterables.cpp
@@ -0,0 +1,37 @@
+#include "output_ip.h"
+
+#include <gtest/gtest.h>
+#include <iostream>
+#include <vector>
+#include <list>
+#include <algorithm>
+#include <sstream>
+#include <string>
+
+using namespace OTUS;
+
+TEST(Utilities, vector_int)
+{
+    std::vector<int> in{1, 2, 3, 4, 5, 6};
+    std::ostringstream str;
+
+    print_ip(in, str, false);
+
+    std::string exp{"1.2.3.4.5.6"};
+    std::string res = str.str();
+    std::cout << "DEBUG" << res << std::endl;
+    ASSERT_EQ(exp, res);
+}
+
+TEST(Utilities, list_int)
+{
+    std::list<int> in{1, 2, 3, 4, 5, 6};
+    std::ostringstream str;
+
+    print_ip(in, str, false);
+
+    std::string exp{"1.2.3.4.5.6"};
+    std::string res = str.str();
+    std::cout << "DEBUG" << res << std::endl;
+    ASSERT_EQ(exp, res);
+}
